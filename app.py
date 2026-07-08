d0f4dd4ffd5b5dcee48707aa6c69bf916ae5aa5d
unknown <rioslezamaseanphillipe@gmail.com>
2026-07-08 01:14:48 -0500

    agrego login y registro

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template, request, jsonify, redirect
+from flask import Flask, render_template, request, jsonify, redirect, session
 
 app = Flask(__name__)
+app.secret_key = "barberiaelite2026"
 
 reservas = []
+usuarios = []
 
 @app.route("/")
 def inicio():
-    return render_template("index.html")
+    if "usuario" not in session:
+        return redirect("/login")
+
+    return render_template("index.html", usuario=session["usuario"])
 
 
 @app.route("/dashboard")
@@ -47,7 +52,55 @@ def api_reserva():
     })
 
     return jsonify({"mensaje": "ok"})
+
 @app.route("/borrar/<int:index>")
 def borrar(index):
     reservas.pop(index)
     return redirect("/dashboard")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    mensaje = ""
+
+    if request.method == "POST":
+        correo = request.form["correo"]
+        password = request.form["password"]
+
+        for usuario in usuarios:
+            if usuario["correo"] == correo and usuario["password"] == password:
+                session["usuario"] = usuario["nombre"]
+                return redirect("/")
+
+        mensaje = "Correo o contraseña incorrectos"
+
+    return render_template("login.html", mensaje=mensaje)
+
+
+@app.route("/registro", methods=["GET", "POST"])
+def registro():
+    if request.method == "POST":
+        nombre = request.form["nombre"]
+        correo = request.form["correo"]
+        password = request.form["password"]
+
+        usuarios.append({
+            "nombre": nombre,
+            "correo": correo,
+            "password": password
+        })
+
+        return redirect("/login")
+
+    return render_template("registro.html")
+
+
+@app.route("/logout")
+def logout():
+    session.pop("usuario", None)
+    return redirect("/login")
+
+
+
+if __name__ == "__main__":
+    print("Estoy entrando al main...")
+    app.run(debug=True)
